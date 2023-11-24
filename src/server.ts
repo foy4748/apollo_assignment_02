7249af5d97a9ce5db14c58553bc36d8802c0517b
foy4748 <faisaljfcl@gmail.com>
2023-11-24 09:59:21 +0600

    Prettier fixed working

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,18 @@ import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
 
-const PORT = process.env.PORT || config.port
+const PORT = process.env.PORT || config.port;
 
 async function main() {
-	try {
-		await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string);
 
-		app.listen(PORT, () => {
-			console.log(`Assignment 2 server is listening on port ${PORT}`);
-		});
-	} catch (err) {
-		console.log(err);
-	}
+    app.listen(PORT, () => {
+      console.log(`Assignment 2 server is listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 main();
